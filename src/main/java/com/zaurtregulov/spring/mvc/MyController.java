214ba4dfb3c50682ba0fab3ef0071e23fbc0aef5
Andrei Cataraga <andreicataraga18@gmail.com>
2023-12-31 23:52:44 +0200

    Adnotarea @RequestMapping pentru clasa Controller

--- src/main/java/com/zaurtregulov/spring/mvc/MyController.java
+++ src/main/java/com/zaurtregulov/spring/mvc/MyController.java
@@ -7,7 +7,50 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
+//@Controller
+//public class MyController {
+//
+//    @RequestMapping("/")
+//    public String showFirstView() {
+//        return "first-view";
+//    }
+//
+//    @RequestMapping("/askDetails")
+//    public String askEmployeeDeatails() {
+//        return "ask-emp-details-view";
+//    }
+//
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(){
+//        return "show-emp-details-view";
+//    }
+//
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(HttpServletRequest request, Model model) {
+//
+//        String empName = request.getParameter("employeeName");
+//        empName = "Mr. " + empName;
+//        model.addAttribute("nameAttribute", empName);
+//        model.addAttribute("description", " - udemy instructor" );
+//
+//        return "show-emp-details-view";
+//    }
+//
+//
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(@RequestParam ("employeeName") String empName, Model model) {
+//
+//        empName = "Mr. " + empName + "!";
+//        model.addAttribute("nameAttribute", empName);
+//        model.addAttribute("description", " - udemy instructor");
+//
+//        return "show-emp-details-view";
+//    }
+//}
+
+
 @Controller
+@RequestMapping("/employee")
 public class MyController {
 
     @RequestMapping("/")
@@ -19,24 +62,6 @@ public class MyController {
     public String askEmployeeDeatails() {
         return "ask-emp-details-view";
     }
-
-//    @RequestMapping("/showDetails")
-//    public String showEmpDetails(){
-//        return "show-emp-details-view";
-//    }
-
-//    @RequestMapping("/showDetails")
-//    public String showEmpDetails(HttpServletRequest request, Model model) {
-//
-//        String empName = request.getParameter("employeeName");
-//        empName = "Mr. " + empName;
-//        model.addAttribute("nameAttribute", empName);
-//        model.addAttribute("description", " - udemy instructor" );
-//
-//        return "show-emp-details-view";
-//    }
-
-
     @RequestMapping("/showDetails")
     public String showEmpDetails(@RequestParam ("employeeName") String empName, Model model) {
 
