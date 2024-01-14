bac32930448d7cc058e19d6a6d7982026ea89020
Andrei Cataraga <andreicataraga18@gmail.com>
2024-01-14 23:40:09 +0200

    Am reanimat tomcatul

--- src/main/java/com/zaurtregulov/spring/mvc/MyController.java
+++ src/main/java/com/zaurtregulov/spring/mvc/MyController.java
@@ -2,30 +2,27 @@ package com.zaurtregulov.spring.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.validation.Valid;
+@Controller
+public class MyController {
 
-//@Controller
-//public class MyController {
-//
-//    @RequestMapping("/")
-//    public String showFirstView() {
-//        return "first-view";
-//    }
-//
-//    @RequestMapping("/askDetails")
-//    public String askEmployeeDeatails() {
-//        return "ask-emp-details-view";
-//    }
-//
+    @RequestMapping("/")
+    public String showFirstView() {
+        return "first-view";
+    }
+
+    @RequestMapping("/askDetails")
+    public String askEmployeeDetails() {
+        return "ask-emp-details-view";
+    }
+
 //    @RequestMapping("/showDetails")
 //    public String showEmpDetails(){
 //        return "show-emp-details-view";
 //    }
-//
+
 //    @RequestMapping("/showDetails")
 //    public String showEmpDetails(HttpServletRequest request, Model model) {
 //
@@ -36,18 +33,18 @@ import javax.validation.Valid;
 //
 //        return "show-emp-details-view";
 //    }
-//
-//
-//    @RequestMapping("/showDetails")
-//    public String showEmpDetails(@RequestParam ("employeeName") String empName, Model model) {
-//
-//        empName = "Mr. " + empName + "!";
-//        model.addAttribute("nameAttribute", empName);
-//        model.addAttribute("description", " - udemy instructor");
-//
-//        return "show-emp-details-view";
-//    }
-//}
+
+
+    @RequestMapping("/showDetails")
+    public String showEmpDetails(@RequestParam("employeeName") String empName, Model model) {
+
+        empName = "Mr. " + empName + "!";
+        model.addAttribute("nameAttribute", empName);
+        model.addAttribute("description", " - udemy instructor");
+
+        return "show-emp-details-view";
+    }
+}
 
 // lectia 61
 //@Controller
@@ -75,44 +72,44 @@ import javax.validation.Valid;
 //}
 
 
-@Controller
-@RequestMapping("/employee")
-public class MyController {
-
-    @RequestMapping("/")
-    public String showFirstView() {
-        return "first-view";
-    }
-
-    @RequestMapping("/askDetails")
-    public String askEmployeeDeatails(Model model) {
-
-//        Employee emp = new Employee;
-//        emp.setName("Mihai");
-//        emp.setSurname("Panfil");
-//        emp.setSalary(900);
-//        model.addAttribute ("employee", emp);
-
-        model.addAttribute("employee", new Employee());
-        return "ask-emp-details-view";
-    }
-
-    @RequestMapping("/showDetails")
-    public String showEmpDetails(@Valid @ModelAttribute("employee") Employee emp
-            , BindingResult bindingResult) {
-
-//        String name = emp.getName();
-//        emp.setName("Mr. "+ name);
-//        String surname = emp.getSurname();
-//        emp.setSurname(surname + "!");
-//        int salary = emp.getSalary();
-//        emp.setSalary(salary*10);
-
-        if (bindingResult.hasErrors()) {
-            return "ask-emp-details-view";
-        }
-        else {
-            return "show-emp-details-view";
-        }
-    }
-}
+//@Controller
+//@RequestMapping("/employee")
+//public class MyController {
+//
+//    @RequestMapping("/")
+//    public String showFirstView() {
+//        return "first-view";
+//    }
+//
+//    @RequestMapping("/askDetails")
+//    public String askEmployeeDeatails(Model model) {
+//
+////        Employee emp = new Employee;
+////        emp.setName("Mihai");
+////        emp.setSurname("Panfil");
+////        emp.setSalary(900);
+////        model.addAttribute ("employee", emp);
+//
+//        model.addAttribute("employee", new Employee());
+//        return "ask-emp-details-view";
+//    }
+//
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(@Valid @ModelAttribute("employee") Employee emp
+//            , BindingResult bindingResult) {
+//
+////        String name = emp.getName();
+////        emp.setName("Mr. "+ name);
+////        String surname = emp.getSurname();
+////        emp.setSurname(surname + "!");
+////        int salary = emp.getSalary();
+////        emp.setSalary(salary*10);
+//
+//        if (bindingResult.hasErrors()) {
+//            return "ask-emp-details-view";
+//        }
+//        else {
+//            return "show-emp-details-view";
+//        }
+//    }
+//}
--- src/main/webapp/WEB-INF/view/ask-emp-details-view.jsp
+++ src/main/webapp/WEB-INF/view/ask-emp-details-view.jsp
@@ -8,13 +8,11 @@
 <br>
 <br>
 
-lectia 61---------------
 <form action="showDetails" method="get">
 <input type="text" name="employeeName"
 placeholder="Write your name"/>
 <input type="submit"/>
 </form>
--------------------------
 
 <form:form action="showDeatails" addAttribute="employee">
 
@@ -28,28 +26,30 @@ placeholder="Write your name"/>
    <form:errors path="salary"/>
    <br><br>
    Department<form:select path:="department"/>
-   lectia 61------------------
+
+
    <form:option value="Information Technology" label="IT"/>
    <form:option value="Human Resources" label="HR"/>
    <form:option value="Sales" label="Sales"/>
-   ------------------
+
    <form:options items="${employee.departmens}"/>
    </form:select>
    <br><br>
    Witch car do you want?
-   lectia 64------------------
+
+
    BMW <form:radiobutton path="carBrand" value="BMW"/>
    Audi <form:radiobutton path="carBrand" value="Audi"/>
    MB <form:radiobutton path="carBrand" value="Mercedes-Benz"/>
-   ------------------
+
    <form:radiobuttons path="carBrand" items="${employee.carBrands}"/>
    <br><br>
    Foreign Language (s)
-   lectia 65---------------
+
    EN <form:checkbox path="languages" value="English"/>
    DE <form:checkbox path="languages" value="Deutch"/>
    FR <form:checkbox path="languages" value="French"/>
-   ------------------
+
    <form:checkboxes path="languages" items="${employee.LanguageList}"/>
    <br><br>
    Phone number <form:input path="phoneNumber"/>
